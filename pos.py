7d15dc094d379cf1b64e9afeb0366720bb0cfa5d
eve-n0rman <n0rman@curiousstranger.org>
2017-11-25 01:13:27 -0700

    Resolves #15 - convert assets from XML to ESI

--- pos.py
+++ pos.py
@@ -25,35 +25,26 @@ def nearest(source, destinations):
 
 def pos_assets():
     corp_id = name_to_id(CORPORATION_NAME, 'corporation')
-    asset_xml = xml_api('/corp/AssetList.xml.aspx',
-                        params={'corporationID': corp_id},
-                        xpath='.//rowset[@name="assets"]/row[@singleton="1"]')
+    assets = esi_api('Assets.get_corporations_corporation_id_assets', token=access_token, corporation_id=corp_id)
     pos = {}
     mods = {}
-    for row in asset_xml:
-        typeID = int(row.get('typeID'))
+    for asset in assets:
+        item_id = int(asset.get('item_id'))
+        # location_flags are a bit of mystery.  Trial and error for now
+        location_flag = asset.get('location_flag')
+        if location_flag not in ['AutoFit']:
+            continue
         # Filter out things that aren't POS mods
-        if typeID not in pos_mods:
+        type_id = int(asset.get('type_id'))
+        if type_id not in pos_mods:
             continue
         # Decorate POS mods with SDE data
-        annotate_element(row, pos_mods[typeID])
-        itemID = int(row.get('itemID'))
-        if row.get('groupName') == 'Control Tower':
+        annotate_element(asset, pos_mods[type_id])
+        if asset.get('groupName') == 'Control Tower':
             pos_stuff = pos
         else:
             pos_stuff = mods
-        pos_stuff[itemID] = row.attrib
-        for contents in row.findall('.//rowset[@name="contents"]/row'):
-            typeID = int(contents.get('typeID'))
-            # Decorate POS mod contents with SDE data
-            if typeID in moon_goo:
-                annotate_element(contents, moon_goo[typeID])
-            elif typeID in fuel_types:
-                annotate_element(contents, fuel_types[typeID])
-            else:
-                continue
-            parents_contents = pos_stuff[itemID].setdefault('contents', [])
-            parents_contents.append(contents.attrib)
+        pos_stuff[item_id] = asset
     locations = item_locations(pos.keys() + mods.keys())
     for itemID, location in locations.iteritems():
         try:
@@ -125,9 +116,10 @@ def check_pos():
         has_stront = False
         has_fuel = False
         has_defensive_mods = False
-        # TODO: handle purposefully offlined POS by checking for fuel
-        for fuel in poses[pos_id].get('contents', []):
-            fuel_type_id = int(fuel.get('typeID'))
+        for fuel in esi_api('Corporation.get_corporations_corporation_id_starbases_starbase_id',
+                            token=access_token, corporation_id=corp_id, starbase_id=pos_id,
+                            system_id=system_id).get('fuels'):
+            fuel_type_id = int(fuel.get('type_id'))
             quantity = int(fuel.get('quantity'))
             multiplier = .75 if sov else 1.0
             rate = pos_fuel[type_id][fuel_type_id] * multiplier
