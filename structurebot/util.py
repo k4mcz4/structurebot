6ac84bdcee4748e7964c3cbb265d958d0909d891
n0rman <n0rman@brave.wtf>
2018-01-25 16:02:08 -0700

    Add ESI caching and modularize setup for testing

--- structurebot/util.py
+++ structurebot/util.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from urlparse import urlparse
 from operator import attrgetter
 from esipy import App, EsiClient, EsiSecurity
 from xml.etree import cElementTree as ET
@@ -7,30 +8,85 @@ from xml.etree import cElementTree as ET
 from config import *
 
 
-esi_path = os.path.abspath(__file__)
-esi_dir_path = os.path.dirname(esi_path)
+def config_esi_cache(cache_url):
+    """Configure ESI cache backend
 
-esi = App.create(esi_dir_path + '/esi.json')
+    Args:
+        cache_url (string): diskcache or redis url
 
-esi_security = EsiSecurity(
-    app=esi,
-    redirect_uri='http://localhost',
-    client_id=CONFIG['SSO_APP_ID'],
-    secret_key=CONFIG['SSO_APP_KEY'],
-)
+    Returns:
+        cache: None or esipy.cache
 
-esi_security.update_token({
-    'access_token': '',
-    'expires_in': -1,
-    'refresh_token': CONFIG['SSO_REFRESH_TOKEN']
-})
+    >>> config_esi_cache('diskcache:/tmp/esipy-diskcache') # doctest: +ELLIPSIS
+    <esipy.cache.FileCache object at 0x...>
+    >>> config_esi_cache('redis://user:password@127.0.0.1:6379/') # doctest: +ELLIPSIS
+    <esipy.cache.RedisCache object at 0x...>
+    """
+    cache = None
+    if cache_url:
+        cache_url = urlparse(cache_url)
+        if cache_url.scheme == 'diskcache':
+            from esipy.cache import FileCache
+            filename = cache_url.path
+            cache = FileCache(path=filename)
+        elif cache_url.scheme == 'redis':
+            from esipy.cache import RedisCache
+            import redis
+            redis_server = cache_url.hostname
+            redis_port = cache_url.port
+            redis_client = redis.Redis(host=redis_server, port=redis_port)
+            cache = RedisCache(redis_client)
+    return cache
 
-esi_client = EsiClient(
-    retry_requests=True, 
-    header={'User-Agent': 'https://github.com/eve-n0rman/structurebot'},
-    raw_body_only=False,
-    security=esi_security
-)
+
+def setup_esi(app_id, app_secret, refresh_token, cache=None):
+    """Set up the ESI client
+    
+    Args:
+        app_id (string): SSO Application ID from CCP
+        app_secret (string): SSO Application Secret from CCP
+        refresh_token (string): SSO refresh token
+        cache (None, optional): esipy.cache instance
+    
+    Returns:
+        tuple: esi app definition, esi client
+
+    >>> setup_esi(CONFIG['SSO_APP_ID'], CONFIG['SSO_APP_KEY'],
+    ...           CONFIG['SSO_REFRESH_TOKEN'], cache) # doctest: +ELLIPSIS
+    (<pyswagger.core.App object ...>, <esipy.client.EsiClient object ...>)
+    """
+    esi_path = os.path.abspath(__file__)
+    esi_dir_path = os.path.dirname(esi_path)
+
+    esi = App.create(esi_dir_path + '/esi.json')
+
+    esi_security = EsiSecurity(
+        app=esi,
+        redirect_uri='http://localhost',
+        client_id=app_id,
+        secret_key=app_secret,
+    )
+
+    esi_security.update_token({
+        'access_token': '',
+        'expires_in': -1,
+        'refresh_token': refresh_token
+    })
+
+    esi_client = EsiClient(
+        retry_requests=True,
+        header={'User-Agent': 'https://github.com/eve-n0rman/structurebot'},
+        raw_body_only=False,
+        security=esi_security,
+        cache=cache
+    )
+
+    return (esi, esi_client)
+
+cache = config_esi_cache(CONFIG['ESI_CACHE'])
+esi, esi_client = setup_esi(CONFIG['SSO_APP_ID'], CONFIG['SSO_APP_KEY'],
+                            CONFIG['SSO_REFRESH_TOKEN'], cache)
+
 
 def name_to_id(name, name_type):
     """Looks up a name of name_type in ESI
