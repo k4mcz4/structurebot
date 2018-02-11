d08a7f658a5dff983698ed325c6ca7216caef65b
eve-n0rman <n0rman@brave.wtf>
2018-02-11 00:55:46 -0700

    Clean up structure-audit for new capabilities

--- structurebot/structure-audit.py
+++ structurebot/structure-audit.py
@@ -3,13 +3,10 @@
 from assets import CorpAssets
 from citadels import Structure
 from config import CONFIG
-from util import access_token, name_to_id
 
 
 if __name__ == '__main__':
-    corp_id = name_to_id(CONFIG['CORPORATION_NAME'], 'corporation')
-    assets = CorpAssets(corp_id)
-    structures = Structure.from_corporation(CONFIG['CORPORATION_NAME'], access_token, assets.assets)
+    structures = Structure.from_corporation(CONFIG['CORPORATION_NAME'])
     total_fuel = 0
     for structure in sorted(structures, key=lambda x: x.name):
         if 'Rented by' in structure.name:
