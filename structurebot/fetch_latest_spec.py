ad9eaff8192c47d1dd1aadc071e18fe2a51ecb27
eve-n0rman <n0rman@brave.wtf>
2018-12-19 14:05:37 -0700

    CCP droppped the old domain unexpectedly

--- structurebot/fetch_latest_spec.py
+++ structurebot/fetch_latest_spec.py
@@ -7,7 +7,7 @@ import json
 import requests
 from esipy import App
 
-app = App.create('https://esi.tech.ccp.is/_latest/swagger.json', strict=True)
+app = App.create('https://esi.evetech.net/_latest/swagger.json', strict=True)
 
 with open('esi.json', 'w') as specfile:
     json.dump(app.dump(), specfile, indent=4)
